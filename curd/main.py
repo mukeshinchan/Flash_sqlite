a700ec266d49d251f765f8a286064132a7f27ea6
Mukeshkannan D <106548923+mukeshinchan@users.noreply.github.com>
2022-08-10 16:24:12 +0530

    Add files via upload

---
+++ curd/main.py
@@ -0,0 +1,8 @@
+import sqlite3 as sql
+con = sql.connect('table.db')
+cur = con.cursor()
+cur.execute("DROP TABLE IF EXISTS users")
+sql ='''CREATE TABLE "users" ("UID"	INTEGER PRIMARY KEY AUTOINCREMENT,"UNAME"	TEXT,"CONTACT"	TEXT)'''
+cur.execute(sql)
+con.commit()
+con.close()
